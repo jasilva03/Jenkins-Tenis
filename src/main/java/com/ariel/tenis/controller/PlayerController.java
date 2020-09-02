217ffdbc445ea170516ca929fc14d55e87a1ff76
Ariel Silva <jasilva03@yahoo.com.ar>
2020-09-02 00:59:11 -0600

    se agregan archivos

--- src/main/java/com/ariel/tenis/controller/PlayerController.java
+++ src/main/java/com/ariel/tenis/controller/PlayerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,14 +13,14 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("players")
+@RequestMapping("/players")
 public class PlayerController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<Player> getAll() {
 
         List<Player> listPlayers = new ArrayList<>(Arrays.asList(new Player("Rolo", "Rojas")));
-        return  listPlayers;
+        return listPlayers;
 
     }
 
